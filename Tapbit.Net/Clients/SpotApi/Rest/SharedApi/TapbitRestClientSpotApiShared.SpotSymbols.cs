8263e745d3186143113abf05d0e1efc0413f40b7
Jkorf <jankorf91@gmail.com>
2026-09-22 15:40:56 +0200

    wip

--- Tapbit.Net/Clients/SpotApi/Rest/SharedApi/TapbitRestClientSpotApiShared.SpotSymbols.cs
+++ Tapbit.Net/Clients/SpotApi/Rest/SharedApi/TapbitRestClientSpotApiShared.SpotSymbols.cs
@@ -16,7 +16,7 @@ namespace Tapbit.Net.Clients.SpotApi
     {
         #region Get Spot Symbols
 
-        async Task<ICallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetSpotSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public SharedSymbolCatalog? SpotSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicId, _api.EnvironmentName, null);
